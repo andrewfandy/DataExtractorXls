7712cc16e5f183e98c09366702a9efb89ef24734
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-09-16 13:32:23 +0700

    chore: remove TransformedExtractedData object

--- model/TransformedExcelData.cs
+++
@@ -1,7 +0,0 @@
-namespace DataExtractorXls;
-
-public class TransformedExcelData
-{
-    // Transformed data to the correspondence fields, for example THE INSURED would be on string Insured
-    // or SAILING DATE would be on DateTime / Date SailingDate 
-}
