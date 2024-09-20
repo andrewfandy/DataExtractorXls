40f86e599ee02f93f4067b9dbbbac104facd7fb2
andrewfandy <andrewfandy13@gmail.com>
2024-09-20 13:24:12 +0700

    refactor: change on Program.cs

--- Program.cs
+++ Program.cs
@@ -7,17 +7,16 @@ internal class Program
 
     static void Main(string[] args)
     {
+        Console.WriteLine("WELCOME TO THE EXTRACTOR ENGINE");
         Run();
     }
 
     public static void Run()
     {
         ConsoleKey key;
-        Console.WriteLine("WELCOME TO THE EXTRACTOR ENGINE");
-        Console.WriteLine("PRESS 'ENTER' TO BEGIN\nPRESS 'Q' or 'ESC' TO EXIT");
+        Console.WriteLine("PRESS 'ENTER' TO PROCESS\nPRESS 'Q' or 'ESC' TO EXIT");
         do
         {
-
             key = Console.ReadKey(intercept: true).Key;
             if (key == ConsoleKey.Enter)
             {
@@ -25,33 +24,25 @@ internal class Program
                 var path = Console.ReadLine();
                 if (!string.IsNullOrEmpty(path))
                 {
-                    var files = RegisterFiles(path);
+                    var files = RegisterExcelFile.RegisterMultipleFiles(path);
                     DataExtract(files);
-                    DataLoad(DataTransformSingleFile(files), DataLoadServicesType.LOAD_JSON_FILE);
+                    DataLoad(DataTransformMultipleFiles(files), DataLoadServicesType.LOAD_JSON_FILE);
                     Console.WriteLine("\n\nProcess Complete\nPress Enter to start again\nPress Q or Escape to exit");
                 }
                 else
                 {
                     Console.WriteLine("Path is null or empty");
+                    continue;
                 }
                 key = Console.ReadKey(intercept: true).Key;
-
-
             }
+
         } while (key != ConsoleKey.Q && key != ConsoleKey.Escape);
 
     }
-    private static List<ExcelFile> RegisterFiles(string path)
-    {
-        var services = new RegisterFileService(path);
-        services.Process();
-        var files = services.ExcelFiles;
-        Console.WriteLine($"Register Completed\nTotal Files: {files!.Count}");
-
-        return files;
 
 
-    }
+
     private static void DataExtract(List<ExcelFile> excelFiles)
     {
         foreach (ExcelFile file in excelFiles)
@@ -60,9 +51,8 @@ internal class Program
             extract.Process();
         }
     }
-    private static void DataTransform(List<ExcelFile> excelFiles)
+    private static void DataTransformSingleFile(List<ExcelFile> excelFiles)
     {
-        Dictionary<string, object> dataSet = new Dictionary<string, object>();
         foreach (ExcelFile file in excelFiles)
         {
             if (file.ExtractedData == null) continue;
@@ -70,46 +60,42 @@ internal class Program
             transform.Process();
         }
     }
-    private static Dictionary<string, object> DataTransformSingleFile(List<ExcelFile> excelFiles)
+    private static Dictionary<string, object> DataTransformMultipleFiles(List<ExcelFile> excelFiles)
     {
         Dictionary<string, object> dataSet = new Dictionary<string, object>();
-        int fileCounterId = 1;
 
         foreach (var file in excelFiles)
         {
-            string counterParse = fileCounterId.ToString();
-            string zero = new string('0', 5 - counterParse.Length);
-            dataSet.Add(zero + counterParse, file.ExtractedData!);
-            fileCounterId++;
+            dataSet.Add(file.Id, file.ExtractedData!);
         }
         return dataSet;
     }
     private static void DataLoad(List<ExcelFile> excelFiles, DataLoadServicesType type)
     {
-        string input = "";
-        while (string.IsNullOrEmpty(input))
+        string path = "";
+        while (string.IsNullOrEmpty(path))
         {
             Console.WriteLine("\nInput the output directory name (the output still on the 'output' directory):");
-            input = Console.ReadLine()!;
+            path = Console.ReadLine()!;
         }
 
         foreach (var file in excelFiles)
         {
             if (file.ExtractedData == null) continue;
-            var load = new DataLoadServices(file, type, input);
+            var load = new DataLoadServices(file, type, path);
             load.Process();
         }
     }
     private static void DataLoad(Dictionary<string, object> dataSet, DataLoadServicesType type)
     {
-        string input = "";
-        while (string.IsNullOrEmpty(input))
+        string path = "";
+        while (string.IsNullOrEmpty(path))
         {
             Console.WriteLine("\nInput the output directory name (the output still on the 'output' directory):");
-            input = Console.ReadLine()!;
+            path = Console.ReadLine()!;
         }
 
-        var load = new DataLoadServices(dataSet, type, input);
+        var load = new DataLoadServices(dataSet, type, path);
         load.Process();
     }
 }
